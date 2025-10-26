975fd883a77a253fe6192e5046ce8f7125855501
marslan-786 <marslansalfias@gmail.com>
2025-10-26 18:55:56 +0500

    Delete routes directory

--- routes/chat.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// sample placeholder routes
-router.get('/status', (req, res) => {
-  res.json({ status: 'ok', message: 'chat API placeholder' });
-});
-
-module.exports = router;
