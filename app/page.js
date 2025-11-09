41f598224421c8afd2ade4f99c90fb8392caec61
marslan-786 <marslansalfias@gmail.com>
2025-11-09 18:23:18 +0500

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,10 @@
+export default function HomePage() {
+  return (
+    <main className="p-4">
+      <h1 className="text-3xl font-bold text-center">
+        الیاس موبائل مال
+      </h1>
+      <p className="text-center mt-4">ویب سائٹ جلد آرہی ہے...</p>
+    </main>
+  );
+}
